2e30f68518bdde84ca6d5bad1ed32688ff2cad83
iomonad <iomonad@riseup.net>
2021-12-01 21:31:12 +0100

    mise a jour des dependances

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject aoc "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Advent Of Code 2021"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure       "1.10.1"]
+                 [cheshire                  "5.10.1"]
+                 [com.rpl/specter           "1.1.3"]]
   :repl-options {:init-ns aoc.core})
