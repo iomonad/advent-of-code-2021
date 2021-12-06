92c90647f6077abfa96cb2d7bea4694e5fa9eb55
iomonad <iomonad@riseup.net>
2021-12-07 00:18:07 +0100

    day06 wip

--- src/aoc/day06.clj
+++ src/aoc/day06.clj
@@ -1,3 +1,10 @@
 (ns aoc.day06
-  (:require [aoc.utils :refer :all]))
+  (:require [aoc.utils :refer :all]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
 
+(defn day06 []
+  (let [input (->> (str/split (slurp (io/resource "day6.txt"))
+                              #",")
+                   (map read-string))]
+    input))
